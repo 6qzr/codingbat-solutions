27126e0e5b0cf716cf771c0a8d613ad78e2af04b
Mohammed Al Abri <striker.m901@gmail.com>
2025-07-25 22:28:48 +0400

    Add solution for Warmup1-startOz

---
+++ Warmup1/startOz.java
@@ -0,0 +1,8 @@
+public class startOz {
+  public String startOz(String str) {
+    String newStr = "";
+    if (str.length() >= 1 && str.charAt(0) == 'o') newStr += "o";
+    if (str.length() >= 2 && str.charAt(1) == 'z') newStr += "z";
+    return newStr;
+  }
+}
