ec7341dffabc20cc7e9bf6965e2c4dba003ed5a8
Mohammed Al Abri <striker.m901@gmail.com>
2025-07-26 11:01:20 +0400

    Add solution for Warmup2-stringTimes

---
+++ Warmup2/stringTimes.java
@@ -0,0 +1,9 @@
+public class stringTimes {
+  public String stringTimes(String str, int n) {
+    String newStr = "";
+    for (int i = 0; i < n; i++) {
+      newStr += str;
+    }
+    return newStr;
+  }
+}
