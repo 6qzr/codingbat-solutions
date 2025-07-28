46ff2579edc43599d36e2dc647c0f4210cda23aa
Mohammed Al Abri <striker.m901@gmail.com>
2025-07-28 20:33:10 +0400

    Add solution for String1-withoutX

---
+++ String1/withoutX.java
@@ -0,0 +1,15 @@
+public class withoutX {
+  public String withoutX(String str) {
+    if (str.length() == 0 || (str.length() == 1 && str.equals("x"))) return ""; 
+    if (str.startsWith("x") && str.endsWith("x")) {
+      return str.substring(1, str.length()-1);
+    }
+    if (str.startsWith("x")) {
+      return str.substring(1, str.length());
+    }
+    if (str.endsWith("x")) {
+      return str.substring(0, str.length()-1);
+    }
+    return str;
+  }
+}
