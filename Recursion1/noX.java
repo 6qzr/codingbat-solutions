8591897287671a75a914a7592ed24c90808a4dd5
Mohammed Al Abri <striker.m901@gmail.com>
2025-08-02 12:04:45 +0400

    Add solution for Recursion1-noX

---
+++ Recursion1/noX.java
@@ -0,0 +1,9 @@
+public class noX {
+  public String noX(String str) {
+    if (str.length() == 0) return "";
+    if (str.substring(0, 1).equals("x")) {
+      return "" + noX(str.substring(1, str.length()));
+    }
+    return str.substring(0, 1) + noX(str.substring(1, str.length()));
+  }
+}
