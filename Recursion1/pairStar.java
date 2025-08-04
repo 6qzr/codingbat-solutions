4152ba69c4f0ab9b329a8588b0b86dec3347bb3a
Mohammed Al Abri <striker.m901@gmail.com>
2025-08-04 11:08:10 +0400

    Add solution for Recursion1-pairStar

---
+++ Recursion1/pairStar.java
@@ -0,0 +1,9 @@
+public class pairStar {
+  public String pairStar(String str) {
+    if (str.length() <= 1) return str;
+    if (str.substring(0, 1).equals(str.substring(1, 2))) {
+      return str.substring(0, 1) + "*" + pairStar(str.substring(1));
+    }
+    return str.substring(0, 1) + pairStar(str.substring(1));
+  }
+}
