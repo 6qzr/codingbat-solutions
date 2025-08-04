0522ea21862dc4d2112f0e7cf7f370aa9ff66a37
Mohammed Al Abri <striker.m901@gmail.com>
2025-08-04 14:31:48 +0400

    Add solution for Recursion1-stringClean

---
+++ Recursion1/stringClean.java
@@ -0,0 +1,9 @@
+public class stringClean {
+  public String stringClean(String str) {
+    if (str.length() <= 1) return str;
+    if (str.substring(0, 1).equals(str.substring(1, 2))) {
+      return  stringClean(str.substring(0, 1) + str.substring(2));
+    }
+    return str.substring(0, 1) + stringClean(str.substring(1));
+  }
+}
