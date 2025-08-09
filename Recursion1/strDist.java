b9631474f09d1bc5418a18bd5a1c8d7cb7aaa30b
Mohammed Al Abri <striker.m901@gmail.com>
2025-08-09 13:47:11 +0400

    Add solution for Recursion1-strDist

---
+++ Recursion1/strDist.java
@@ -0,0 +1,16 @@
+public class strDist {
+  public int strDist(String str, String sub) {
+    if (str.length() < sub.length()) return 0;
+    if (str.startsWith(sub) && str.endsWith(sub)) {
+      return str.length();
+    }
+    if (str.startsWith(sub)) {
+      return strDist(str.substring(0, str.length() - 1), sub);
+    }
+    if (str.endsWith(sub)) {
+      return strDist(str.substring(1), sub);
+    }
+    if (str.length() <= sub.length()) return 0;
+    return strDist(str.substring(1, str.length() - 1), sub);
+  }
+}
