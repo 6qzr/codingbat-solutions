008de1ae8065168363e595c810ca2c53a66dd7cb
Mohammed Al Abri <striker.m901@gmail.com>
2025-08-11 13:37:36 +0400

    Add solution for Logic2-closeFar

---
+++ Logic2/closeFar.java
@@ -0,0 +1,15 @@
+public class closeFar {
+  public boolean closeFar(int a, int b, int c) {
+    if (Math.abs(a - b) <= 1) {
+      if (Math.abs(a - c) >= 2 && Math.abs(b - c) >= 2) {
+        return true;
+      }
+    }
+    if (Math.abs(a - c) <= 1) {
+      if (Math.abs(a - b) >= 2 && Math.abs(c - b) >= 2) {
+        return true;
+      }
+    }
+    return false;
+  }
+}
